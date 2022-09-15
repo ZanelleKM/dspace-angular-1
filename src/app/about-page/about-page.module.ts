b4d1664aab9642f2caa50e1c8f9f2a7f17048398
Zanele <user@users-MacBook-Pro.local>
2022-09-15 13:52:35 +0200

    update Dockerfile and README file with build/push a docker image instructions

--- src/app/about-page/about-page.module.ts
+++ src/app/about-page/about-page.module.ts
@@ -3,11 +3,6 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
 import { AboutPageComponent } from './about-page.component';
 import { AboutPageRoutingModule } from './about-page.routing.module';
-// import { CommunityListPageRoutingModule } from './community-list-page.routing.module';
-// import { CommunityListComponent } from './community-list/community-list.component';
-// import { ThemedCommunityListPageComponent } from './themed-community-list-page.component';
-// import { ThemedCommunityListComponent } from './community-list/themed-community-list.component';
-
 
 const DECLARATIONS = [
   AboutPageComponent,
